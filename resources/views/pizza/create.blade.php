906972160ad908640451dd6f80b7450bedbd07b8
nika jibladze <nikajibladze5@gmail.com>
2021-11-10 00:42:51 +0400

    pizza add feature

--- resources/views/pizza/create.blade.php
+++ resources/views/pizza/create.blade.php
@@ -15,19 +15,26 @@
                    <!--{{ __('You are logged in!') }}-->
                 </div>
             </div>
-        </div> 
+            @if (count($errors) > 0)
 
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Pizza</div>
-                @if (count($errors) > 0)
+            <div class="card mt-5">
+                <div class="card-body">
                 <div class="alert alert-danger">
                     @foreach ($errors->all() as $error)
                        <p> {{ $error }} <p>
                         @endforeach
                 </div>
-                @endif
-            <form action="{{route('pizza.store')}}" method="post">@csrf
+             </div>
+            </div>
+            @endif
+
+        </div> 
+
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Pizza</div>
+        
+            <form action="{{route('pizza.store')}}" method="post" enctype="multipart/form-data">@csrf
                 <div class="card-body">
                     <div class="form-group">
                         <label for="name">Name of pizza</label>
